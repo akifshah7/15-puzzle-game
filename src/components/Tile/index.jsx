3c9e572939ad9b9ad671b3da07e02183eb115e91
Akif <akifshah7@gmail.com>
2024-05-29 17:19:53 +0530

    formatting

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -1,20 +1,23 @@
-import './index.css';
-import PropTypes from 'prop-types';
+import "./index.css";
+import PropTypes from "prop-types";
 
-const Tile = ({ number, moveTile }) => 
-    <button 
-        onClick={() => moveTile(number)} 
-        className={`number ${number.value === number.index + 1? 'correct' : ''} ${number.value===16? 'disabled' : ''} slot--${number.index}`}>
-        {number.value === 16? '' : number.value}
-    </button>
+const Tile = ({ number, moveTile }) => (
+  <button
+    onClick={() => moveTile(number)}
+    className={`number ${number.value === number.index + 1 ? "correct" : ""} ${
+      number.value === 16 ? "disabled" : ""
+    } slot--${number.index}`}
+  >
+    {number.value === 16 ? "" : number.value}
+  </button>
+);
 
 Tile.propTypes = {
-    number: PropTypes.shape({
-        value: PropTypes.number.isRequired,
-        index: PropTypes.number.isRequired,
-    }).isRequired,
-    moveTile: PropTypes.func.isRequired,
-};    
-
+  number: PropTypes.shape({
+    value: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired,
+  }).isRequired,
+  moveTile: PropTypes.func.isRequired,
+};
 
 export default Tile;
